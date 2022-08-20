45016ebcaf521f0b02911ac787ac82c2e8911119
bbompk <panithiv@gmail.com>
2022-08-20 23:43:04 +0700

    add base classes for board loading and playing cell

---
+++ Assets/Scripts/Playing Board/PlayingCell.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayingCell : MonoBehaviour
+{
+
+    public static readonly Dictionary<string, int> Direction = new Dictionary<string, int>()
+    {
+        { "NE", 0 },
+        { "E", 1 },
+        { "SE", 2 },
+        { "SW", 3 },
+        { "W", 4 },
+        { "NW", 5 },
+    };
+
+    public List<PlayingCell> neighbors = new List<PlayingCell>(6);
+
+
+    private Vector2Int coord;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
